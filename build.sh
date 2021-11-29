14c83795cbcdf0a0188b800f3a3eaa26349d5a59
Karl Dinwiddie <karldinwiddie@gmail.com>
2021-11-28 19:55:40 -0800

    .

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 . ./loadprops_linux.sh
-rm -rf %HTTP_HOME%
-mkdir %HTTP_HOME%
+rm -rf ${HTTP_HOME}
+mkdir ${HTTP_HOME}
 rm -rf target
 mvn -f shoring clean install
 echo 'Linking, this will take a minute...'
--- shoring/compat/src/main/java/com/panopset/compat/AppVersion.java
+++ shoring/compat/src/main/java/com/panopset/compat/AppVersion.java
@@ -1,7 +1,7 @@
 package com.panopset.compat;
 
 public class AppVersion {
-  public static final String VERS = "1.0.8";
+  public static final String VERS = "1.0.9";
 
   public static String getVersion() {
     return VERS;
