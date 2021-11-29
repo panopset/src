9604303b62d0b4113788a0d36add0eee99ba59d5
Karl Dinwiddie <karldinwiddie@gmail.com>
2021-11-29 03:29:31 -0800

    .

--- shoring/desk/src/main/java/module-info.java
+++ shoring/desk/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.panopset.desk {
   requires transitive com.panopset.blackjackEngine;
+  requires transitive com.panopset.minpin;
   requires transitive com.panopset.flywheel;
   requires transitive com.panopset.lowerclass;
   requires transitive com.panopset.fxapp;
@@ -8,6 +9,7 @@ module com.panopset.desk {
   requires javafx.swing;
 
   opens com.panopset.marin.games.blackjack to javafx.fxml;
+  opens com.panopset.marin.games.minpin to javafx.fxml;
   opens com.panopset.marin.apps.fw to javafx.fxml;
   opens com.panopset.marin.apps.gr to javafx.fxml;
   opens com.panopset.marin.apps.lc to javafx.fxml;
