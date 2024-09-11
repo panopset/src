dea6deed86057e80374ad3c8d2867e2faaab5b2c
Karl Dinwiddie <karldinwiddie@gmail.com>
2024-09-11 05:38:23 -0700

    .

--- code/paldeb.sh
+++ code/paldeb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. ./checkProps4Linux.sh
-rsync -avuzh  ./target/* $TGT_HTML/downloads/
-rsync -avuzh  ~/panopset.jar $TGT_HTML/downloads/$PLATFORM_KEY/
-rsync -avuzh  ./legacy/target/*.json $TGT_HTML/downloads/
+. ./checkProps4LinuxDEB.sh
+scp ./target/* $TGT_HTML/downloads/
+scp ~/panopset.jar $TGT_HTML/downloads/$PLATFORM_KEY/
+scp ./legacy/target/*.json $TGT_HTML/downloads/
--- code/paw.sh
+++ code/paw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 . ./checkProps4Win.sh
-echo scp ./target/* $TGT_HTML/downloads/
 scp ./target/* $TGT_HTML/downloads/
 scp ~/panopset.jar $TGT_HTML/downloads/$PLATFORM_KEY/
+scp ./legacy/target/*.json $TGT_HTML/downloads/
